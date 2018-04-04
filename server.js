27575db8a9e156d10b3750eacfe9c62d3709e850
Glitch (nomics-lametric) <none>
2018-04-04 19:41:45 +0000

    🍮🏎 Updated with Glitch

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ var https = require('https');
 var express = require('express');
 var app = express();
 
+var respondError = function(res, error) {
+  console.log(error);
+  res.status(500).send(error);
+}
+
 app.get("/", function (req, res) {
-  console.log(req.query);
-  getData().then((data) => {
-    res.json(frames(req.query, data));
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send(error);
-  });
+  console.log(req.method, req.originalUrl);
+  getData('/v0/dashboard').then((data) => {
+    getData('/v0/market-cap/sparkline').then((mkt) => {
+      res.json(frames(req.query, data, mkt));
+    }).catch(respondError.bind(this, res));
+  }).catch(respondError.bind(this, res));
 });
 
-var getData = function() {
+var getData = function(path) {
   return new Promise((resolve, reject) => {
-    https.get('https://api.nomics.com/v0/dashboard/day', (res) => {
+    https.request({hostname: 'api.nomics.com', path: path, headers: {Referer: 'nomics-lametric.glitch.me'}}, (res) => {
       const { statusCode } = res;
       const contentType = res.headers['content-type'];
 
@@ -45,16 +49,17 @@ var getData = function() {
       });
     }).on('error', (e) => {
       reject(e);
-    });
+    }).end();
   });
 }
 
-var frames = function(opts, data) {
-  var btc = data.find((c) => { return c.Currency == "BTC"; });
-  var bch = data.find((c) => { return c.Currency == "BCH"; });
-  var eth = data.find((c) => { return c.Currency == "ETH"; });
-  var ltc = data.find((c) => { return c.Currency == "LTC"; });
-  var xrp = data.find((c) => { return c.Currency == "XRP"; });
+var frames = function(opts, data, mkt) {
+  var btc = data.find((c) => { return c.CurrencyID == "BTC"; });
+  var bch = data.find((c) => { return c.CurrencyID == "BCH"; });
+  var eth = data.find((c) => { return c.CurrencyID == "ETH"; });
+  var ltc = data.find((c) => { return c.CurrencyID == "LTC"; });
+  var xrp = data.find((c) => { return c.CurrencyID == "XRP"; });
+  var icon = iconFromDelta(mkt)
   var formatter = new Intl.NumberFormat(
     'en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0}
   );
@@ -62,24 +67,44 @@ var frames = function(opts, data) {
     'en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 4, minimumFractionDigits: 0, maximumSignificantDigits: 4, useGrouping: false}
   );
   var fs = [];
-  fs.push({text: "Nomics", icon: "i15855"});
-  if (opts.btc !== "0") {
-    fs.push({text: formatter.format(btc.Close), icon: "i12460"});
+  fs.push({text: "Nomics", icon: icon});
+  if (opts.btc !== "false") {
+    fs.push({text: formatter.format(btc.Close), icon: "i10814"});
   }
-  if (opts.bch !== "0") {
+  if (opts.bch !== "false") {
     fs.push({text: formatter.format(bch.Close), icon: "i12303"});
   }
-  if (opts.eth !== "0") {
+  if (opts.eth !== "false") {
     fs.push({text: formatter.format(eth.Close), icon: "i11862"});
   }
-  if (opts.ltc !== "0") {
+  if (opts.ltc !== "false") {
     fs.push({text: formatter.format(ltc.Close), icon: "i11792"});
   }
-  if (opts.xrp !== "0") {
+  if (opts.xrp !== "false") {
     fs.push({text: smallFormatter.format(xrp.Close), icon: "i15863"});
   }
   
   return {frames: fs};
+}
+
+var iconFromDelta = function(mkt) {
+  var closes = mkt.day[0].closes
+  var open = closes[0]
+  var close = closes[closes.length-1]
+  var delta = (close-open)/open
+  if (delta > 0.10) {
+    return "i17824"
+  }
+  if (delta > 0.05) {
+    return "i17825"
+  }
+  if (delta >= -0.05) {
+    return "i15855"
+  }
+  if (delta >= -0.10) {
+    return "i17826"
+  }
+  return "i17827"
 }
 
 var listener = app.listen(process.env.PORT, function () {
